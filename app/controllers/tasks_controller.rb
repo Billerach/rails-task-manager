e3292e6e0275fda725a0e7ad754c67226865444a
Pascal Billerach <p.billerach@gmail.com>
2022-11-15 16:01:09 +0100

    j'en ai marre

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,26 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to show_path(@task)
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
   # CREATE
-  # get "tasks/new", to: "tasks#new", as: "new_task"
-  # post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new", as:"new"
+  post "tasks", to: "tasks#create"
 
   # READ
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: "show"
 
   # UPDATE
-  # get "tasks/:id/edit", to: "tasks#edit", as: "edit_tasks"
-  # patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch "tasks/:id", to: "tasks#update"
 
   # # # DELETE
   # delete "tasks/:id", to: "tasks#destroy"
